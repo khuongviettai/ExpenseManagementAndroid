40ebb3d94bd16afc1415196fcd8d91469ec8f9c8
khuongviettai <khuongviettai@outlook.com>
2023-03-10 22:40:33 +0700

    first commit

---
+++ app/src/main/java/com/khuongviettai/expensemanager/ExpenseModel.java
@@ -0,0 +1,82 @@
+package com.khuongviettai.expensemanager;
+
+import java.io.Serializable;
+
+public class ExpenseModel implements Serializable {
+    private String expenseID;
+    private String note;
+    private String category;
+    private String type;
+    private long amount;
+    private long time;
+    private String uid;
+
+    public ExpenseModel() {
+    }
+
+    public ExpenseModel(String expenseID, String note, String category, String type, long amount, long time, String uid) {
+        this.expenseID = expenseID;
+        this.note = note;
+        this.category = category;
+        this.type = type;
+        this.amount = amount;
+        this.time = time;
+        this.uid = uid;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getExpenseID() {
+        return expenseID;
+    }
+
+    public void setExpenseID(String expenseID) {
+        this.expenseID = expenseID;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public long getAmount() {
+        return amount;
+    }
+
+    public void setAmount(long amount) {
+        this.amount = amount;
+    }
+
+    public long getTime() {
+        return time;
+    }
+
+    public void setTime(long time) {
+        this.time = time;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+}
---
+++ app/src/main/java/com/khuongviettai/expensemanager/OnItemsClick.java
@@ -0,0 +1,5 @@
+package com.khuongviettai.expensemanager;
+
+public interface OnItemsClick {
+    void onClick(ExpenseModel expenseModel);
+}
